dc5e359c8dfa7cf1378d651c3f53b556ff9e923a
rachelharu <rachel.haru9@gmail.com>
2021-10-08 21:51:04 -0700

    added JS if else statement to check if email input is valid

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-document.getElementById("btn").addEventListener("click", function() {
-    console.log("hi");
-}
+// checks if email field is valid
+const emailField = document.getElementById('email');
 
-)
+
+emailField.addEventListener('keyup', function(event) {
+  const isValidEmail = emailField.checkValidity();
+
+
+if (isValidEmail) {
+  console.log("omg");
+} else {
+  console.log("no");
+}
+});
+
+// console logs hi when button is clicked
+document.getElementById("btn").addEventListener("click", function() {
+  console.log("hi");
+
+});
