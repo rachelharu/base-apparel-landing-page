b30109d172905036e34e505bf77294718fd1ca62
rachelharu <rachel.haru9@gmail.com>
2021-10-11 19:19:30 -0700

    added the error elements using css html and JS

--- app.js
+++ app.js
@@ -2,17 +2,27 @@
 const emailField = document.getElementById('email');
 const signUpForm = document.getElementById('contact-form');
 const submitButton = document.getElementById('btn');
-
+const errorImage = document.getElementById('error-image')
+//checks validity of email field
 emailField.addEventListener('keyup', function(event) {
   const isValidEmail = emailField.checkValidity();
 
+//function to show error message and change the css of id error
+  function showError(message){
+  let errorElement = document.getElementById("error");
+  errorElement.innerHTML = message;
+  errorElement.style.display = 'block';
+  }
 
+//if else statement to submit the form if it passes all checks and enables button if valid
 if (isValidEmail) {
   console.log("omg");
   submitButton.disabled = false;
 } else {
   console.log("no");
   submitButton.disabled = true;
+  showError('Please provide a valid email.');
+  errorImage.style.visibility = 'visible';
 }
 });
 
